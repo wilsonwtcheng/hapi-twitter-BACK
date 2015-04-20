e4bffabdb04f22c72b9830ac64c53f1fb5d835f4
Wilson Cheng <wilzon@gmail.com>
2015-04-20 11:01:11 +0800

    tweets.js works

--- routes/tweets.js
+++ routes/tweets.js
@@ -122,6 +122,7 @@ exports.register = function(server, options, next) {
       }
     },
 
+
   ]);
 
   next();
@@ -131,3 +132,82 @@ exports.register.attributes = {
   name: 'tweets-route',
   version: '0.0.1'
 };
+
+
+
+
+//    { //Retrieve all tweets WC
+//      method: 'GET',
+//      path: '/tweets',
+//      handler: function(request, reply) {
+//       var db = request.server.plugins['hapi-mongodb'].db;
+//       db.collection("tweets").find().toArray(function(err, users) {
+//         if(err) { 
+//           return reply('Internal MongoDB error', err); 
+//         }
+//           reply(users);
+//           console.log("great success! All the tweets.");
+//       })
+//      }
+//    },
+
+//     { //get tweets WC
+//       method: "GET",
+//       path: "/tweets/{wooWooWoo}", 
+//       handler: function(request, reply) {
+//         var id       = encodeURIComponent(request.params.wooWooWoo); //make sure it doesn't have weird stuff inside. you need the encode thing if you want to use MONGO.        
+//         var db       = request.server.plugins['hapi-mongodb'].db;    
+//         var ObjectID = request.server.plugins['hapi-mongodb'].ObjectID;    
+
+//         db.collection("tweets").findOne( {"_id" :ObjectID(id) }, function(err, obtainedTweet) { //inside the function, err is the err, the second thing is the result.
+//         if (err) throw err;
+//         reply(obtainedTweet);
+//           console.log("Obtained Tweet! success!")
+//       });    
+//       }
+//     }, 
+
+// //**
+//   { //WC WIP OF TWEET
+//     method: 'POST',
+//     path: '/tweet',
+//     config: {
+//     handler: function(request, reply) { //ALWAYS request reply here.
+//       var newUser = request.payload.userInfo;
+//       var db = request.server.plugins['hapi-mongodb'].db;
+
+//       Bcrypt.genSalt(10, function(err, salt){
+//         Bcrypt.hash(newUser.password, salt, function(err, hash){ //newUser.password accesses the password
+//           newUser.password = hash;
+//           var uniqueUserQuery = {
+//             $or: [ //or, count the number of matching username or matching email.
+//               {username: newUser.username},
+//               {email: newUser.email}
+//             ]
+//           };          
+//           db.collection("users").count(uniqueUserQuery, function(err, userExist){ //if user exists, don't do it, if it doesn't exist, create user.
+//             if(userExist) {
+//               return reply("username or email already exists", err);
+//             }
+//             db.collection("users").insert(newUser, function(err, writeResult){
+//               if(err) {
+//                 return reply("you suck ass, usercreate FAIL");
+//               }
+//                 reply(writeResult);
+//                 console.log("awesome man!")
+//             });
+//           });
+//         })
+//       });
+//     },
+//     validate: {
+//         payload: {
+//           userInfo: {
+//             username: Joi.string().min(3).max(20).required(), 
+//             email: Joi.string().email().max(50).required(),  
+//             password: Joi.string().min(3).max(20).required(),
+//           }
+//         }
+//     }
+//     }
+//   },
